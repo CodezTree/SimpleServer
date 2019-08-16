7097f377f7d236562c10967380fcfa5766065cdf
Changjun Mun <codeztree@naver.com>
2019-08-16 17:31:39 +0900

    Home Fixed

--- Service/urls.py
+++ Service/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('health/', views.check_health, name="health"),
     path('articlecomp.action.keyword/', views.articlecomp_action_keyword, name="artcomp_keyword" ),
+    path('articlecomp.action.keyword', views.articlecomp_action_keyword, name="artcomp_keyword" ),
     path('articlecomp.action.now/', views.articlecomp_action_now, name="artcomp_now"),
+    path('articlecomp.action.now', views.articlecomp_action_now, name="artcomp_now"),
 ]
